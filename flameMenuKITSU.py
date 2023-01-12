cf1bc10362ea5b29f3d8b3dc9c5a143f728859dd
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-01-12 23:12:07 +0000

    update flameMenuKITSU.py xz

--- flameMenuKITSU.py
+++ flameMenuKITSU.py
@@ -15,7 +15,7 @@ import re
 from pprint import pprint
 from pprint import pformat
 
-__version__ = 'v0.0.1 dev 004'
+__version__ = 'v0.0.1 dev 005'
 
 menu_group_name = 'KITSU'
 app_name = 'flameMenuKITSU'
